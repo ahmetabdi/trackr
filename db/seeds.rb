bdc7e256b9859232a0e7d609dd6771d56603bf06
Ahmet <ahmetabdi@gmail.com>
2017-03-23 21:52:51 +0000

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+category = Category.create(name: 'Electronics & Computers')
+
+SubCategory.create([
+  { name: 'TV & Home Cinema' },
+  { name: 'Sat Nav & Car Electronics' },
+  { name: 'Phones & Accessories' }
+])
+
+category = Category.create(name: 'Home, Garden, Pets & DIY')
+
+SubCategory.create([
+  { name: 'Kitchen & Bathroom Fixtures' },
+  { name: 'Trade & Professional Tools' },
+  { name: 'All DIY & Tools' }
+])
+
+category = Category.create(name: 'Movies, TV, Music & Games')
+
+SubCategory.create([
+  { name: 'DVD & Blu-ray' },
+  { name: 'CDs & Vinyl' },
+  { name: 'PC & Video Games' }
+])
