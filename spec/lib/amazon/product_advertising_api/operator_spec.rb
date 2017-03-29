aa8a0dcac85d817b1dd73a8f4defd5b689fa8455
Ahmet <ahmetabdi@gmail.com>
2017-03-29 22:13:35 +0100

    .

--- spec/lib/amazon/product_advertising_api/operator_spec.rb
+++ spec/lib/amazon/product_advertising_api/operator_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Amazon::ProductAdvertisingApi::Operator, :vcr do
   describe '.item_lookup' do
     describe 'searches by ASIN as a default' do
       let(:asin) { 'B01AX72ZTI' }
-
       let(:item_lookup) { described_class.item_lookup(asin) }
 
       it 'should return a ItemLookup object' do
