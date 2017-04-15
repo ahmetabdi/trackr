9d1a106b12d2c1f434c7422f5548a7914c2365d2
Ahmet <ahmetabdi@gmail.com>
2017-04-15 13:26:11 +0100

    Disable redis connections in TEST ENV

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,9 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV.fetch('REDIS_HOST')}:#{ENV.fetch('REDIS_PORT')}" }
-end
+unless Rails.env.test?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "redis://#{ENV.fetch('REDIS_HOST')}:#{ENV.fetch('REDIS_PORT')}" }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV.fetch('REDIS_HOST')}:#{ENV.fetch('REDIS_PORT')}" }
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "redis://#{ENV.fetch('REDIS_HOST')}:#{ENV.fetch('REDIS_PORT')}" }
+  end
 end
--- config/initializers/traffic_control.rb
+++ config/initializers/traffic_control.rb
@@ -1,6 +1,8 @@
-ActiveJob::TrafficControl.client = ConnectionPool.new(size: 5, timeout: 5) {
-  Redis.new(
-    host: ENV.fetch('REDIS_HOST'),
-    port: ENV.fetch('REDIS_PORT')
-  )
-}
+unless Rails.env.test?
+  ActiveJob::TrafficControl.client = ConnectionPool.new(size: 5, timeout: 5) {
+    Redis.new(
+      host: ENV.fetch('REDIS_HOST'),
+      port: ENV.fetch('REDIS_PORT')
+    )
+  }
+end
