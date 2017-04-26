ef1ad76d5495a5f7d93b9b21edd3798696f8d0db
Ahmet <ahmetabdi@gmail.com>
2017-04-26 10:02:24 +0100

    Remove file_watch when under windows OS controlled through ENV var

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  # TODO: See why this breaks code reloading
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  unless ENV.fetch('WINDOWS')
+    config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  end
 end
