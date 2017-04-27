43c067d0b4e7acc543a1cfdc72a9eb042a1814ed
Ahmet <ahmetabdi@gmail.com>
2017-04-27 23:12:57 +0100

    Stuff

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Dotenv::Railtie.load
 
 module Trackr
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.autoload_paths += %W(#{config.root}/lib)
     config.eager_load_paths += %W(#{config.root}/lib)
     config.active_job.queue_adapter = :sidekiq
