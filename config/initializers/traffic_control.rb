711abf0a4b617d93bf4b6b80e9508fcde910b8c2
Ahmet <ahmetabdi@gmail.com>
2017-05-04 22:54:18 +0100

    Remove last key

--- config/initializers/traffic_control.rb
+++ config/initializers/traffic_control.rb
@@ -1,8 +1,5 @@
 unless Rails.env.test?
   ActiveJob::TrafficControl.client = ConnectionPool.new(size: 5, timeout: 5) {
-    Redis.new(
-      host: ENV.fetch('REDIS_HOST'),
-      port: ENV.fetch('REDIS_PORT')
-    )
+    Redis.new
   }
 end
