90e941a04b6634610675099eb05dcb43967e5d8c
Ahmet <ahmetabdi@gmail.com>
2017-05-16 09:28:09 +0100

    Truncate titles using dotdotdot.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,9 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function() {
+$( document ).on('turbolinks:load', function() {
   $("#lightgallery").lightGallery();
-});
+  $(".dotdotdot").dotdotdot({
+    height: 65
+  });
+})
