7d796fe3ffb88d6fdedaa7d0fe7ba53826d99c6e
Ahmet <ahmetabdi@gmail.com>
2017-05-28 00:18:32 +0100

    Add product reindexer task

---
+++ app/jobs/product_indexer_job.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+class ProductIndexerJob < ApplicationJob
+  queue_as :high_priority
+
+  def perform
+    AmazonProduct.reindex
+  end
+end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -19,3 +19,11 @@ if Sidekiq::Cron::Job.find('find_and_update_products').nil?
     class: 'ProductUpdaterJob'
   )
 end
+
+if Sidekiq::Cron::Job.find('reindex_products').nil?
+  Sidekiq::Cron::Job.create(
+    name: 'reindex_products',
+    cron: '*/15 * * * *',
+    class: 'ProductIndexerJob'
+  )
+end
