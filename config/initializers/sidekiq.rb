acdcbd7767b791e286b0e5341b3ba33d66be120b
Ahmet <ahmetabdi@gmail.com>
2017-06-05 10:59:28 +0100

    Set sidekiq cron to run every hour

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,20 +10,20 @@ unless Rails.env.test?
   Sidekiq.configure_client do |config|
     config.redis = { url: ENV.fetch('REDIS_URL') }
   end
-end
 
-if Sidekiq::Cron::Job.find('find_and_update_products').nil?
-  Sidekiq::Cron::Job.create(
-    name: 'find_and_update_products',
-    cron: '*/15 * * * *',
-    class: 'ProductUpdaterJob'
-  )
-end
+  if Sidekiq::Cron::Job.find('find_and_update_products').nil?
+    Sidekiq::Cron::Job.create(
+      name: 'find_and_update_products',
+      cron: '*/60 * * * *',
+      class: 'ProductUpdaterJob'
+    )
+  end
 
-if Sidekiq::Cron::Job.find('reindex_products').nil?
-  Sidekiq::Cron::Job.create(
-    name: 'reindex_products',
-    cron: '*/15 * * * *',
-    class: 'ProductIndexerJob'
-  )
+  if Sidekiq::Cron::Job.find('reindex_products').nil?
+    Sidekiq::Cron::Job.create(
+      name: 'reindex_products',
+      cron: '*/60 * * * *',
+      class: 'ProductIndexerJob'
+    )
+  end
 end
