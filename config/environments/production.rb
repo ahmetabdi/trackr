160e56adf4918156e7a3ff89675401e3207a1c11
Ahmet <ahmetabdi@gmail.com>
2017-06-18 03:45:00 -0700

    Add asset host to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,17 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { host: ENV.fetch('APP_HOST') }
+  config.action_mailer.asset_host = ENV.fetch('APP_HOST')
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com'
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
