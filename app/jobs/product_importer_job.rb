bf5218a0e6fe11da3337c6118f6e66ad91eea5c6
Ahmet <ahmetabdi@gmail.com>
2017-06-20 12:06:17 +0100

    Rescue AmazonThrottleLimit

--- app/jobs/product_importer_job.rb
+++ app/jobs/product_importer_job.rb
@@ -3,9 +3,9 @@
 class ProductImporterJob < ApplicationJob
   queue_as :low_priority
 
-  # rescue_from(AmazonThrottleLimit) do |_|
-  #   retry_job wait: 3.seconds, queue: :low_priority
-  # end
+  rescue_from(AmazonThrottleLimit) do |_|
+    retry_job wait: 30.seconds, queue: :low_priority
+  end
 
   def perform(asin)
     Amazon::ProductAdvertisingApi::Scanners::ProductScanner.run(asin)
