379ca88269e4e70934969533ef767b82342d46c0
Ahmet <ahmetabdi@gmail.com>
2017-06-21 08:28:24 +0100

    Add cron tasks back in

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 require 'sidekiq/web'
-# require 'sidekiq-cron'
-# require 'sidekiq/cron/web'
+require 'sidekiq-cron'
+require 'sidekiq/cron/web'
 
 unless Rails.env.test?
   Sidekiq.configure_server do |config|
@@ -11,19 +11,19 @@ unless Rails.env.test?
     config.redis = { url: ENV.fetch('REDIS_URL') }
   end
 
-  # if Sidekiq::Cron::Job.find('find_and_update_products').nil?
-  #   Sidekiq::Cron::Job.create(
-  #     name: 'find_and_update_products',
-  #     cron: '*/60 * * * *',
-  #     class: 'ProductUpdaterJob'
-  #   )
-  # end
+  if Sidekiq::Cron::Job.find('find_and_update_products').nil?
+    Sidekiq::Cron::Job.create(
+      name: 'find_and_update_products',
+      cron: '*/720 * * * *',
+      class: 'ProductUpdaterJob'
+    )
+  end
 
-  # if Sidekiq::Cron::Job.find('reindex_products').nil?
-  #   Sidekiq::Cron::Job.create(
-  #     name: 'reindex_products',
-  #     cron: '*/60 * * * *',
-  #     class: 'ProductIndexerJob'
-  #   )
-  # end
+  if Sidekiq::Cron::Job.find('reindex_products').nil?
+    Sidekiq::Cron::Job.create(
+      name: 'reindex_products',
+      cron: '*/720 * * * *',
+      class: 'ProductIndexerJob'
+    )
+  end
 end
