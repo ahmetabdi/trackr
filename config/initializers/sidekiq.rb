5462759b50c15d1238b597d7d2060f9c34df0e4e
Ahmet <ahmetabdi@gmail.com>
2017-06-24 07:12:54 -0700

    Remove sidekiq-cron

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,20 +10,4 @@ unless Rails.env.test?
   Sidekiq.configure_client do |config|
     config.redis = { url: ENV.fetch('REDIS_URL') }
   end
-
-  if Sidekiq::Cron::Job.find('find_and_update_products').nil?
-    Sidekiq::Cron::Job.create(
-      name: 'find_and_update_products',
-      cron: '*/60 * * * *',
-      class: 'ProductUpdaterJob'
-    )
-  end
-
-  if Sidekiq::Cron::Job.find('reindex_products').nil?
-    Sidekiq::Cron::Job.create(
-      name: 'reindex_products',
-      cron: '*/60 * * * *',
-      class: 'ProductIndexerJob'
-    )
-  end
 end
