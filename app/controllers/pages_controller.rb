9d1982a288e32f39904da726c2e38a4481a211f7
Ahmet <ahmetabdi@gmail.com>
2017-06-28 11:14:54 -0700

    5 minimum

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,20 +4,20 @@ class PagesController < ApplicationController
   def home
     @amazon_product_categories = AmazonProductCategory.alphabetical
     @amazon_product_groups = AmazonProductGroup.alphabetical
-    @amazon_products = AmazonProduct.where('current_sales_rank <= 500').includes(:amazon_product_group).order('percentage_to_save DESC').page(params[:page]).per(12)
+    @amazon_products = AmazonProduct.where('current_sales_rank <= 500 AND current_price >= 500').includes(:amazon_product_group).order('percentage_to_save DESC').page(params[:page]).per(12)
   end
 
   def popular
     @amazon_product_categories = AmazonProductCategory.alphabetical
     @amazon_product_groups = AmazonProductGroup.alphabetical
-    @amazon_products = AmazonProduct.where('current_sales_rank <= 500').includes(:amazon_product_group).order('percentage_to_save DESC').page(params[:page]).per(12)
+    @amazon_products = AmazonProduct.where('current_sales_rank <= 500 AND current_price >= 500').includes(:amazon_product_group).order('percentage_to_save DESC').page(params[:page]).per(12)
     prepare_meta_tags(title: 'Popular', reverse: true)
   end
 
   def largest_savings
     @amazon_product_categories = AmazonProductCategory.alphabetical
     @amazon_product_groups = AmazonProductGroup.alphabetical
-    @amazon_products = AmazonProduct.includes(:amazon_product_group).order('percentage_to_save DESC').page(params[:page]).per(12)
+    @amazon_products = AmazonProduct.includes(:amazon_product_group).where('current_price >= 500').order('percentage_to_save DESC').page(params[:page]).per(12)
     prepare_meta_tags(title: 'Largest Savings', reverse: true)
   end
 
